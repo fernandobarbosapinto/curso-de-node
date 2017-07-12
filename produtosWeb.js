a386e9b26eb0a9c1f5eea424d2a765e2e9b4adca
fernando barbosa <fpinto@igcorp.com.br>
2017-07-12 13:51:22 -0300

    Comecando o curso de node subindo o servidor - ajustes

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,9 +1,13 @@
 var http = require('http');
+var porta = 3000;
+var ip = 'localhost';
 
 var server = http.createServer(function(req, res){
+	console.log('Recebendo request');
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('<html><body><h1>Listando os produtos</h1></body></html>');
 });
 
-server.listen(3000);
+server.listen(porta, ip);
 
-console.log('servidor rodando');
+console.log('servidor rodando em http://' + ip + ':' + porta + '/');
