a83db6800bb565dc2e0ecb46fae9f10e2f232d2e
fernando barbosa <fpinto@igcorp.com.br>
2017-07-13 18:37:05 -0300

    Configurando para consultar os dados do banco

--- livraria/app/routes/produto.js
+++ livraria/app/routes/produto.js
@@ -1,6 +1,19 @@
 module.exports = function(app){
 	app.get('/produtos', function(req, res){
-		console.log('Listando...');
-		res.render('produtos/lista');
+		//console.log('Listando...');
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : 'Mudar123',
+			database : 'livraria_nodejs'
+		});
+
+		connection.query('select * from produtos', function(err, results){
+			res.send(results)
+		});
+
+		connection.end();
+		
 	});
 }
