36570627802fd84dd8b24ef1beba6dec087731c4
fernando barbosa <fpinto@igcorp.com.br>
2017-07-19 16:43:10 -0300

    Formulario para cadastro e verbos http

--- livraria/app/routes/produto.js
+++ livraria/app/routes/produto.js
@@ -1,18 +1,37 @@
 //var connectionFactory = require('../infra/connectionFactory');
 
 module.exports = function(app){
-	app.get('/produtos', function(req, res){
+	var listarProdutos = function(req, res){
 		//console.log('Listando...');
 
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
 		
-		produtosBanco.lista(function(err, results){
+		produtosDAO.lista(function(err, results){
 			//res.send(results)
 			res.render('produtos/lista',{lista:results});
 		});
 
 		connection.end();
 		
+	}
+
+	app.get('/produtos', listarProdutos);
+
+	app.get('/produtos/cadastro', function(req, res){
+		res.render('produtos/cadastro');
+	});
+
+	app.post('/produtos/salva', function(req, res){
+		var produto = req.body;
+		
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		
+		produtosDAO.salva(produto, function(erros, results){
+			res.redirect('/produtos');
+		});
+
+		connection.end();
 	});
 }
