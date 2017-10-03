78ba525be699ba531811260a309402d9c342a2de
fernando barbosa <fpinto@igcorp.com.br>
2017-10-03 17:34:41 -0300

    Finalizando Curso de Node

--- livraria/app.js
+++ livraria/app.js
@@ -1,7 +1,11 @@
 var app = require('./config/express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('io', io);
 
 //var rotasProdutos = require('./app/routes/produto')(app);
 
-app.listen(3000, function(){
+http.listen(3000, function(){
 	console.log("Servidor rodando");
 });
--- livraria/config/express.js
+++ livraria/config/express.js
@@ -18,5 +18,20 @@ module.exports = function(){
 		.then('infra')
 		.into(app);
 
+	app.use(function(req, res, next){
+		res.status(404).render('erros/404');
+	});
+
+	app.use(function(error, req, res, next){
+		if(!process.env.NODE_ENV || process.env.NODE_ENV === 'dev'){
+			res.status(500).render('erros/500');
+			return;
+		}
+		next(error);
+	});
+
+	//Lembre que a ordem dos Middlewares importa para que quando um seja carregado, 
+	//o express já tenha carregado todas as outras dependências das quais ele necessita.
+	
 	return app;
 }
